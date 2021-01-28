60237bdbd085983de8f0d346f1527a752bb13418
shinhyungjin <shinhyungjin@naver.com>
2021-01-28 19:18:42 +0900

    01.28 Android 커밋 ( Swing의 Menu바는 안드로이드에서 ActionBar이며 햄거버 더보기 아이콘이 Menu임 이는 res에 menu 폴더가 있고 menu 전용 resourcefile이 있음 xml에 보여주기 위해서는 onCreateOptionsMenu를 오버로딩하고 MenuInflater객체로 인플레이터 해줘야함, Menu Item에 이벤트를 주기 위해선 onOptionsItemSelected를 오버로딩, fragment 환경 구성 1. xml에 ViewPager id부여,  2. MyViewPagerAdpater Class 생성 및 FragmentPagerAdapter 구현,   3. MainActivity에 ViewPager 생성, 4. MainActivity에 MyViewPagerAdapter 생성 및 생성자 호출, fragment 는 ViewPager를 이용해서 연결하며, Xml에 ViewPager를 작성하고 Activity에서 논리구성  - ViewPager는 ViewPagerAdapter를 구현해야함, myViewPagerAdapter = new MyViewPagerAdapter(this.getSupportFragmentManager(), 0); , mp3 파일은 resource 폴더중 raw로 만들어서 사용 (상수처리 되기때문에 int로 받으면 사용가능), 내부 익명 클래스 함수는 지역변수에 접근할 수 없으나 JS는 클로저, 자바는 람다식을 이용하면 접근가능, MP3 Fragmemt : MediaPlayer, 람다식, MusicVO 이용, Chatting Fragment : ChatThread, Handler, Socket, Thread, SwingServerSocket 이용 ( N:N 채팅 ), Gallery Fragment : Node.Js( Http, static[정적자원 처리 미들웨어], express[외부모듈 이므로 npm필요] 모듈이용 ) )

---
+++ 01.28/app/src/main/java/com/koreait/actionbarapp/MainActivity.java
@@ -0,0 +1,52 @@
+package com.koreait.actionbarapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    ViewPager viewPager;
+    MyViewPagerAdapter myViewPagerAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        viewPager = (ViewPager)findViewById(R.id.viewPager);
+        myViewPagerAdapter = new MyViewPagerAdapter(this.getSupportFragmentManager(), 0);
+        viewPager.setAdapter(myViewPagerAdapter);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = this.getMenuInflater();
+        menuInflater.inflate(R.menu.navi,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        String msg = "";
+        switch(item.getItemId()) {
+            case android.R.id.home : msg="Home"; break;
+            case R.id.mp3 : showPage(0); break;
+            case R.id.chat : showPage(1); break;
+            case R.id.gallery : showPage(2); break;
+            case R.id.setting : showPage(3); break;
+        }
+        //Toast.makeText(this,"msg : " + msg, Toast.LENGTH_SHORT).show();
+        return true;
+    }
+
+    public void showPage(int position) {
+        viewPager.setCurrentItem(position,true);
+    }
+}
---
+++ 01.28/app/src/main/java/com/koreait/actionbarapp/chat/ChatFragment.java
@@ -0,0 +1,96 @@
+package com.koreait.actionbarapp.chat;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.koreait.actionbarapp.R;
+
+import java.io.IOException;
+import java.net.Socket;
+
+public class ChatFragment extends Fragment {
+    Socket socket;
+    EditText t_ip, t_port, t_input;
+    TextView t_log;
+    Thread thread;
+    ChatThread chatThread;
+    Button bt_send;
+    Handler handler;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_chat, container, false);
+
+        t_log = (TextView) view.findViewById(R.id.t_log);
+        t_ip = (EditText) view.findViewById(R.id.t_ip);
+        t_port = (EditText) view.findViewById(R.id.t_port);
+        t_input = (EditText) view.findViewById(R.id.t_input);
+
+        bt_send = (Button) view.findViewById(R.id.bt_send);
+        Button bt_connect = (Button) view.findViewById(R.id.bt_connect);
+
+        handler = new Handler(Looper.getMainLooper()){
+            @Override
+            public void handleMessage(@NonNull Message message) {
+                Bundle bundle = message.getData();
+                String msg = bundle.getString("msg");
+                t_log.append(msg+"\n");
+            }
+        };
+
+        bt_connect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                connectServer();
+            }
+        });
+        bt_send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                send();
+            }
+        });
+
+        return view;
+    }
+
+    public void connectServer() {
+        thread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    socket = new Socket(t_ip.getText().toString(), Integer.parseInt(t_port.getText().toString()));
+                    chatThread = new ChatThread(socket, ChatFragment.this);
+                    chatThread.start();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        };
+        thread.start();
+    }
+
+    public void send() {
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+
+            }
+        };
+        thread.start();
+    }
+}
